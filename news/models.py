9d3bf0ac969b901d4ab5cdddaa0115d03d0a871c
Hem <flyinghem@gmail.com>
2016-07-16 00:36:18 +0545

    Added views, template, static file ex. CSS.

--- news/models.py
+++ news/models.py
@@ -12,7 +12,16 @@ class NewsItem(models.Model):
     author = models.ForeignKey('auth.User')
     news_title = models.CharField(max_length=200)
     text = models.TextField()
-    published_date = models.CharField(max_length=50)
+    created_date = models.DateTimeField(
+        default=timezone.now
+    )
+    published_date = models.DateTimeField(
+        blank=True, null=True
+    )
+
+    def publish(self):
+        self.published_date - timezone.now()
+        self.save()
 
     def __str__(self):
         return self.news_title
--- news/views.py
+++ news/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import NewsItem
+from django.utils import timezone
 
 # Create your views here.
+
+
+def news_list(request):
+    # Queryset to Order news according to published date
+    # news is name of Queryset
+    news = NewsItem.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'news/news_list.html', {'news': news})
+
