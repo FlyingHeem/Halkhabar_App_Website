4b4703e6ac105d45f968092c4a8067fbb4c25f63
Hem <flyinghem@gmail.com>
2016-07-16 00:58:50 +0545

    Created Date Removed

--- news/models.py
+++ news/models.py
@@ -1,20 +1,19 @@
 from __future__ import unicode_literals
 
-
 from django.db import models
 from django.utils import timezone
+
 
 # model for news
 
 
 class NewsItem(models.Model):
-
     author = models.ForeignKey('auth.User')
     news_title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-        default=timezone.now
-    )
+    # created_date = models.DateTimeField(
+    #    default=timezone.now
+    # )
     published_date = models.DateTimeField(
         blank=True, null=True
     )
